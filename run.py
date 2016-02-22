49841435e7ebbe2b7d126dd1de10bf6809ed1a49
Dennis Chan <dennischan@ucla.edu>
2016-02-21 20:50:05 -0800

    Add run.py

---
+++ run.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+
+import argparse
+import pyshark
+import signal, sys
+import collections
+
+class PacketStats:
+	window_size = 1
+
+	def __init__(self, packet_type):
+		self.packet_type = packet_type
+		self.packets = collections.deque()
+		self.total_length = 0
+
+	def add_packet(self, packet):
+		if packet.length >= 0:
+			self.packets.append({'timestamp': float(packet.sniff_timestamp), 'length': float(packet.length)})
+			self.total_length += float(packet.length)
+			while float(packet.sniff_timestamp) - self.packets[0]['timestamp'] > self.window_size:
+				self.total_length -= self.packets[0]['length']
+				self.packets.popleft()
+
+def signal_handler(signal, frame):
+	for packet_type in packet_stats:
+		print packet_type + ': ' + str(packet_stats[packet_type].total_length)
+	sys.exit(0)
+
+##########
+
+parser = argparse.ArgumentParser()
+parser.add_argument('interface', help='interface to capture')
+args = parser.parse_args()
+
+capture = pyshark.LiveCapture(interface=args.interface)
+
+packet_stats = {'arp': PacketStats('arp'), 'icmp': PacketStats('icmp'), 'tcp': PacketStats('tcp'), 'udp': PacketStats('udp'), 'other': PacketStats('other')}
+
+signal.signal(signal.SIGINT, signal_handler)
+
+for packet in capture.sniff_continuously():
+	if hasattr(packet, 'tcp'):
+		packet_stats['tcp'].add_packet(packet) 
+	elif hasattr(packet, 'udp'):
+		packet_stats['udp'].add_packet(packet) 
+	elif hasattr(packet, 'arp'):
+		packet_stats['arp'].add_packet(packet) 
+	elif hasattr(packet, 'icmp'):
+		packet_stats['icmp'].add_packet(packet) 
+	else:
+		packet_stats['other'].add_packet(packet) 
