3af3a031cf714068bb0e251061421696da09a5b5
Dennis Chan <dennischan@ucla.edu>
2016-04-04 18:37:33 -0700

    Add recv_json.py

---
+++ recv_json.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+
+import argparse
+import signal
+import sys
+import subprocess
+import os
+import shlex
+import re
+import json
+import socket
+
+def recv_json(args):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((args.address, args.port))
+    s.listen(1)
+
+    conn, addr = s.accept()
+    while True:
+        data = conn.recv(1024)
+        if not data:
+            break
+        print data
+    conn.close()
+
+def signal_handler(signal, frame):
+    sys.exit(0)
+
+def main():
+    signal.signal(signal.SIGINT, signal_handler)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('address', help='bind IP address')
+    parser.add_argument('port', type=int, help='bind port')
+    args = parser.parse_args()
+
+    recv_json(args)
+
+if __name__ == '__main__':
+    main()
