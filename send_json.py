ff24f22f61188280e8618af82e42695dac61355e
Dennis Chan <dennischan@ucla.edu>
2016-04-06 22:13:30 -0700

    Add protobufs

--- send_json.py
+++ send_json.py
@@ -5,19 +5,22 @@ import signal
 import sys
 import subprocess
 import os
-import shlex
-import re
 import json
 import socket
+import update_pb2
 
 def send_json(args):
     with open(os.path.join(args.output_dir, args.json_file)) as json_file:
         json_obj = json.load(json_file)
         json_str = json.dumps(json_obj)
 
+        update = update_pb2.Update()
+        update.type = update_pb2.Update.FLOW
+        update.data = json_str
+
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect((args.address, args.port))
-        s.send(json_str)
+        s.send(update.SerializeToString())
         s.close()
 
 def signal_handler(signal, frame):
