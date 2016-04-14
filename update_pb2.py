eb4263663f594e6a8768884e6b81b7c9c3f0a5b7
Dennis Chan <dennischan@ucla.edu>
2016-04-13 17:30:29 -0700

    Add more fields to update.proto

--- update_pb2.py
+++ update_pb2.py
@@ -13,7 +13,7 @@ from google.protobuf import descriptor_pb2
 DESCRIPTOR = _descriptor.FileDescriptor(
   name='update.proto',
   package='sdn',
-  serialized_pb='\n\x0cupdate.proto\x12\x03sdn\"b\n\x06Update\x12$\n\x04type\x18\x01 \x02(\x0e\x32\x16.sdn.Update.UpdateType\x12\x0c\n\x04\x64\x61ta\x18\x02 \x02(\t\"$\n\nUpdateType\x12\x0c\n\x08NEIGHBOR\x10\x00\x12\x08\n\x04\x46LOW\x10\x01\"%\n\x06\x42undle\x12\x1b\n\x06update\x18\x01 \x03(\x0b\x32\x0b.sdn.Update')
+  serialized_pb='\n\x0cupdate.proto\x12\x03sdn\"l\n\x06Update\x12$\n\x04type\x18\x01 \x02(\x0e\x32\x16.sdn.Update.UpdateType\x12\x0c\n\x04\x64\x61ta\x18\x02 \x02(\t\".\n\nUpdateType\x12\x08\n\x04PING\x10\x00\x12\x0c\n\x08NEIGHBOR\x10\x01\x12\x08\n\x04\x46LOW\x10\x02\"H\n\x06\x42undle\x12\x11\n\ttimestamp\x18\x01 \x02(\x05\x12\x0e\n\x06origin\x18\x02 \x02(\t\x12\x1b\n\x06update\x18\x03 \x03(\x0b\x32\x0b.sdn.Update')
 
 
 
@@ -24,18 +24,22 @@ _UPDATE_UPDATETYPE = _descriptor.EnumDescriptor(
   file=DESCRIPTOR,
   values=[
     _descriptor.EnumValueDescriptor(
-      name='NEIGHBOR', index=0, number=0,
+      name='PING', index=0, number=0,
       options=None,
       type=None),
     _descriptor.EnumValueDescriptor(
-      name='FLOW', index=1, number=1,
+      name='NEIGHBOR', index=1, number=1,
+      options=None,
+      type=None),
+    _descriptor.EnumValueDescriptor(
+      name='FLOW', index=2, number=2,
       options=None,
       type=None),
   ],
   containing_type=None,
   options=None,
   serialized_start=83,
-  serialized_end=119,
+  serialized_end=129,
 )
 
 
@@ -71,7 +75,7 @@ _UPDATE = _descriptor.Descriptor(
   is_extendable=False,
   extension_ranges=[],
   serialized_start=21,
-  serialized_end=119,
+  serialized_end=129,
 )
 
 
@@ -83,8 +87,22 @@ _BUNDLE = _descriptor.Descriptor(
   containing_type=None,
   fields=[
     _descriptor.FieldDescriptor(
-      name='update', full_name='sdn.Bundle.update', index=0,
-      number=1, type=11, cpp_type=10, label=3,
+      name='timestamp', full_name='sdn.Bundle.timestamp', index=0,
+      number=1, type=5, cpp_type=1, label=2,
+      has_default_value=False, default_value=0,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None),
+    _descriptor.FieldDescriptor(
+      name='origin', full_name='sdn.Bundle.origin', index=1,
+      number=2, type=9, cpp_type=9, label=2,
+      has_default_value=False, default_value=unicode("", "utf-8"),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None),
+    _descriptor.FieldDescriptor(
+      name='update', full_name='sdn.Bundle.update', index=2,
+      number=3, type=11, cpp_type=10, label=3,
       has_default_value=False, default_value=[],
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
@@ -98,8 +116,8 @@ _BUNDLE = _descriptor.Descriptor(
   options=None,
   is_extendable=False,
   extension_ranges=[],
-  serialized_start=121,
-  serialized_end=158,
+  serialized_start=131,
+  serialized_end=203,
 )
 
 _UPDATE.fields_by_name['type'].enum_type = _UPDATE_UPDATETYPE
